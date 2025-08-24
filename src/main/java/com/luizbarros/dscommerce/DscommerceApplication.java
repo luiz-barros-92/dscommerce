d352e9c397cf3e0ece20e374edf81482f3abdfe5
Luiz Barros <lck.barros@gmail.com>
2025-08-24 18:48:42 -0300

    feat: create Product and Category entities and its relations
    feat: set the initial mensage of server ON in main application

--- src/main/java/com/luizbarros/dscommerce/DscommerceApplication.java
+++ src/main/java/com/luizbarros/dscommerce/DscommerceApplication.java
@@ -1,13 +1,18 @@
 package com.luizbarros.dscommerce;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscommerceApplication {
+public class DscommerceApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server ON!");
+	}
 }
---
+++ src/main/java/com/luizbarros/dscommerce/entities/Category.java
@@ -0,0 +1,65 @@
+package com.luizbarros.dscommerce.entities;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Category {
+
+	@Id
+	private Long id;
+	private String name;
+	
+	@ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+	
+	public Category() {
+	}
+
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}	
+}
