9e7d46473d7bc06f4c0ead3f51c10b89b17d6597
Luiz Barros <lck.barros@gmail.com>
2025-08-24 12:31:44 -0300

    first commit

---
+++ src/main/java/com/luizbarros/dscommerce/DscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.luizbarros.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+}
