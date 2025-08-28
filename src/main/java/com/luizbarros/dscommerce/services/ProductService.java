12cce017677f6163636e255d0783eff2810fdcca
Luiz Barros <lck.barros@gmail.com>
2025-08-28 20:35:59 -0300

    feat: create ProductService and ProductRepository
    feat: create findById method in ProductService

---
+++ src/main/java/com/luizbarros/dscommerce/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.luizbarros.dscommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luizbarros.dscommerce.dto.ProductDTO;
+import com.luizbarros.dscommerce.entities.Product;
+import com.luizbarros.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional
+	public ProductDTO findById(Long id) {
+		Product product = repository.findById(id).get();
+		return new ProductDTO(product);
+	}
+}
