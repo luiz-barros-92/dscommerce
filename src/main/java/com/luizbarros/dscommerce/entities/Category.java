652c630238ce3380f5c328961baa397eafb433cf
Luiz Barros <lck.barros@gmail.com>
2025-08-28 21:03:27 -0300

    fix: add strategy to auto generation of id in Category

--- src/main/java/com/luizbarros/dscommerce/entities/Category.java
+++ src/main/java/com/luizbarros/dscommerce/entities/Category.java
@@ -5,6 +5,8 @@ import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
@@ -14,6 +16,7 @@ import jakarta.persistence.Table;
 public class Category {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
