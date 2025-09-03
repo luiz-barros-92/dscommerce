5276b87546df6676185241ce211a3eea5edd4a53
Luiz Barros <lck.barros@gmail.com>
2025-09-02 22:06:28 -0300

    feat: Bean validations

--- src/main/java/com/luizbarros/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/luizbarros/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,6 +27,13 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(DatabaseException.class)
 	public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.BAD_REQUEST;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<CustomError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
