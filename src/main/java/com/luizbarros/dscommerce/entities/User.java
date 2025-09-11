b505d10e15060bc4c65fe9b236e859fdb44d0576
Luiz Barros <lck.barros@gmail.com>
2025-09-10 21:29:21 -0300

    feat: implement Role entity with relations with User

--- src/main/java/com/luizbarros/dscommerce/entities/User.java
+++ src/main/java/com/luizbarros/dscommerce/entities/User.java
@@ -1,13 +1,18 @@
 package com.luizbarros.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +29,12 @@ public class User {
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+	        joinColumns = @JoinColumn(name = "user_id"),
+	        inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 	
 	public User() {
 	}
@@ -84,7 +95,20 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+	
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		 return false;
+	}
+ 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
