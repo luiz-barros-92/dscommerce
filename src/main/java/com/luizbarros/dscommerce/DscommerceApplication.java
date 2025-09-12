a2610e4113cd949126f3ac30b6f8b5efeeabd2cf
Luiz Barros <lck.barros@gmail.com>
2025-09-11 21:07:07 -0300

    feat: implement BCrypt password
    fix: insert hash password in db

--- src/main/java/com/luizbarros/dscommerce/DscommerceApplication.java
+++ src/main/java/com/luizbarros/dscommerce/DscommerceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DscommerceApplication implements CommandLineRunner{
-
+		
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
--- src/main/java/com/luizbarros/dscommerce/config/SecurityConfig.java
+++ src/main/java/com/luizbarros/dscommerce/config/SecurityConfig.java
@@ -3,11 +3,18 @@ package com.luizbarros.dscommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable());
