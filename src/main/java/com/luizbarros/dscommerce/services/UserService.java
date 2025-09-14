6808ccdbf07b493b60d89997de95bccd866246c9
Luiz Barros <lck.barros@gmail.com>
2025-09-13 22:08:10 -0300

    implement UserDetailsService interface

---
+++ src/main/java/com/luizbarros/dscommerce/services/UserService.java
@@ -0,0 +1,36 @@
+package com.luizbarros.dscommerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.luizbarros.dscommerce.entities.Role;
+import com.luizbarros.dscommerce.entities.User;
+import com.luizbarros.dscommerce.projections.UserDetailsProjection;
+import com.luizbarros.dscommerce.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService{
+	
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+		if(result.size() == 0) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		User user = new User();
+		user.setEmail(username);
+		user.setPassword(result.get(0).getPassword());
+		for(UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		return user;
+	}
+}
