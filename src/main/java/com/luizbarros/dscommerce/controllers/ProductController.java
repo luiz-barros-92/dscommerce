130a9ca2c73dfff0ebdbb2ca2fed7f04ed57b385
Luiz Barros <lck.barros@gmail.com>
2025-09-22 20:52:30 -0300

    feat: implement role authorization in ProductController

--- src/main/java/com/luizbarros/dscommerce/controllers/ProductController.java
+++ src/main/java/com/luizbarros/dscommerce/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ public class ProductController {
 	@Autowired
 	private ProductService service;
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		return ResponseEntity.ok(service.findById(id));
@@ -41,6 +43,7 @@ public class ProductController {
 		return ResponseEntity.ok(service.findAll(name, pageable)) ;
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
