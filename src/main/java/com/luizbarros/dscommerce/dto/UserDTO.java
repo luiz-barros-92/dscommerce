bca0203840e46fdedf357ba3be9e65f50f9fca7d
Luiz Barros <lck.barros@gmail.com>
2025-09-25 22:26:49 -0300

    feat: implement UserDTO
    fix: imeplement getRoles() method in User to use it in UserDTO

---
+++ src/main/java/com/luizbarros/dscommerce/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.luizbarros.dscommerce.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.luizbarros.dscommerce.entities.Role;
+import com.luizbarros.dscommerce.entities.User;
+
+public record UserDTO(
+		Long id,
+	    String name,
+	    String email,
+	    String phone,
+	    LocalDate birthDate,
+	    List<String> roles
+) {
+	public UserDTO(User entity) {
+		this(
+			entity.getId(),
+			entity.getName(),
+			entity.getEmail(),
+			entity.getPhone(),
+			entity.getBirthDate(),
+			entity.getRoles().stream().map(Role::getAuthority).toList()
+		);
+	}
+}
