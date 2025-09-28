4c2c358e68a61725130517d902c78977e5192d28
Luiz Barros <lck.barros@gmail.com>
2025-09-28 19:29:47 -0300

    feat: implement OrderController, OrderRepository, OrderService
    feat: get order by id

---
+++ src/main/java/com/luizbarros/dscommerce/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.luizbarros.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luizbarros.dscommerce.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
