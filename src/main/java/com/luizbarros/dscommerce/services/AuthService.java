b6f4ef4de859b8baf61267d1ea0e2c0a7cb139af
Luiz Barros <lck.barros@gmail.com>
2025-09-30 22:49:47 -0300

    feat: validate self or admin to find Order by id endpoint

---
+++ src/main/java/com/luizbarros/dscommerce/services/AuthService.java
@@ -0,0 +1,21 @@
+package com.luizbarros.dscommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizbarros.dscommerce.entities.User;
+import com.luizbarros.dscommerce.services.exceptions.ForbiddenException;
+
+@Service
+public class AuthService {
+
+	@Autowired
+	private UserService userService;
+	
+	public void validateSelfOrAdmin(long userId) {
+		User user = userService.authenticated();
+		if(!user.hasRole("ROLE_ADMIN") && !user.getId().equals(userId)) {
+			throw new ForbiddenException("Access denied");
+		}
+	}
+}
