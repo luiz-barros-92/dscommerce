bddcd7bbba77ef04dedb7c72f85348ec58eb5de7
Luiz Barros <lck.barros@gmail.com>
2025-12-29 00:18:44 -0300

    test: ProductService findAll paged

--- src/test/java/com/luizbarros/dscommerce/services/ProductServiceTests.java
+++ src/test/java/com/luizbarros/dscommerce/services/ProductServiceTests.java
@@ -3,8 +3,10 @@ package com.luizbarros.dscommerce.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,9 +14,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.luizbarros.dscommerce.dto.ProductDTO;
+import com.luizbarros.dscommerce.dto.ProductMinDTO;
 import com.luizbarros.dscommerce.entities.Product;
 import com.luizbarros.dscommerce.repositories.ProductRepository;
 import com.luizbarros.dscommerce.services.exceptions.ResourceNotFoundException;
@@ -32,6 +39,7 @@ public class ProductServiceTests {
 	private Long existingProductId, nonExistingProductId;
 	private String productName;
 	private Product product;
+	private PageImpl<Product> page;
 	
 	@BeforeEach
 	void setUp() throws Exception {
@@ -39,9 +47,11 @@ public class ProductServiceTests {
 		nonExistingProductId = 10000L;
 		productName = "Playstation 5";
 		product = ProductFactory.createProduct(productName);
+		page = new PageImpl<>(List.of(product));
 		
 		when(repository.findById(existingProductId)).thenReturn(Optional.of(product));
 		when(repository.findById(nonExistingProductId)).thenReturn(Optional.empty());
+		when(repository.searchByName(any(), (Pageable)any())).thenReturn(page);
 	}
 	
 	@Test
@@ -57,6 +67,15 @@ public class ProductServiceTests {
 		assertThrows(ResourceNotFoundException.class, () -> {
 			service.findById(nonExistingProductId);
 		});
+	}
+	
+	@Test
+	public void findAllShouldReturnPagedProductMinDTO() {
+		Pageable pageable = PageRequest.of(0, 12);		
+		Page<ProductMinDTO> result = service.findAll(productName, pageable);
+		assertNotNull(result);
+		assertEquals(result.getSize(), 1);
+		assertEquals(result.iterator().next().name(), productName);
 	}
 
 }
