389729673dc1af6ba75312e46bc429f1f41fc966
Luiz Barros <lck.barros@gmail.com>
2026-01-18 00:37:14 -0300

    test: implement unit tests for loadUserByUsername

--- src/test/java/com/luizbarros/dscommerce/services/UserServiceTests.java
+++ src/test/java/com/luizbarros/dscommerce/services/UserServiceTests.java
@@ -1,16 +1,27 @@
 package com.luizbarros.dscommerce.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.luizbarros.dscommerce.entities.User;
 import com.luizbarros.dscommerce.projections.UserDetailsProjection;
 import com.luizbarros.dscommerce.repositories.UserRepository;
+import com.luizbarros.dscommerce.tests.UserDetailsFactory;
+import com.luizbarros.dscommerce.tests.UserFactory;
 
 @ExtendWith(SpringExtension.class)
 public class UserServiceTests {
@@ -29,8 +40,25 @@ public class UserServiceTests {
 	void setup() throws Exception {
 		existingUsername = "abreu@gmail.com";
 		nonExistingUsername = "nemeu@gmail.com";
-		//TODO
-
+		
+		user = UserFactory.createCustomClientUser(1L, existingUsername);
+		userDetails = UserDetailsFactory.createCustomAdmintUser(existingUsername);
+		
+		when(repository.searchUserAndRolesByEmail(existingUsername)).thenReturn(userDetails);
+		when(repository.searchUserAndRolesByEmail(nonExistingUsername)).thenReturn(new ArrayList<>());
 	}
-
+	
+	@Test
+	public void loadUserByUsernameShouldReturnUserDetailsWhenUserExists() {
+		UserDetails result = service.loadUserByUsername(existingUsername);
+		assertNotNull(result);
+		assertEquals(result.getUsername(), existingUsername);		
+	}
+	
+	@Test
+	public void loadUserByUsernameShouldThrowUserNotFoundExceptionWhenUserDoesNotExist() {
+		assertThrows(UsernameNotFoundException.class, () -> {
+			service.loadUserByUsername(nonExistingUsername);
+		});
+	}
 }
