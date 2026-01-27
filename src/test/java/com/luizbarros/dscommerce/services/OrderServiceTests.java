d771c2c3082d2fffa88a09ec2ac0b0ecca408efe
Luiz Barros <lck.barros@gmail.com>
2026-01-26 22:24:49 -0300

    test: OrderService insert

--- src/test/java/com/luizbarros/dscommerce/services/OrderServiceTests.java
+++ src/test/java/com/luizbarros/dscommerce/services/OrderServiceTests.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -15,16 +16,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.luizbarros.dscommerce.dto.OrderDTO;
 import com.luizbarros.dscommerce.entities.Order;
+import com.luizbarros.dscommerce.entities.Product;
 import com.luizbarros.dscommerce.entities.User;
+import com.luizbarros.dscommerce.repositories.OrderItemRepository;
 import com.luizbarros.dscommerce.repositories.OrderRepository;
+import com.luizbarros.dscommerce.repositories.ProductRepository;
 import com.luizbarros.dscommerce.services.exceptions.ForbiddenException;
 import com.luizbarros.dscommerce.services.exceptions.ResourceNotFoundException;
 import com.luizbarros.dscommerce.tests.OrderFactory;
+import com.luizbarros.dscommerce.tests.ProductFactory;
 import com.luizbarros.dscommerce.tests.UserFactory;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class OrderServiceTests {
@@ -37,16 +45,30 @@ public class OrderServiceTests {
 
 	@Mock
 	private AuthService authService;
+	
+	@Mock
+	private ProductRepository productRepository;
+	
+	@Mock
+	private OrderItemRepository orderItemRepository;
+	
+	@Mock
+	private UserService userService;
 
 	private Long existingOrderId, nonExistingOrderId;
+	private Long existingProductId, nonExistingProductId;
 	private Order order;
 	private OrderDTO orderDTO;
 	private User admin, client;
+	private Product product;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		existingOrderId = 1L;
 		nonExistingOrderId = 2L;
+		
+		existingProductId = 1L;
+		nonExistingProductId = 2L;
 
 		admin = UserFactory.createCustomAdminUser(1L, "Ana");
 		client = UserFactory.createCustomClientUser(2L, "Bob");
@@ -54,8 +76,16 @@ public class OrderServiceTests {
 		order = OrderFactory.createOrder(client);
 		orderDTO = new OrderDTO(order);
 
+		product = ProductFactory.createProduct();
+		
 		when(repository.findById(existingOrderId)).thenReturn(Optional.of(order));
 		when(repository.findById(nonExistingOrderId)).thenReturn(Optional.empty());
+		
+		when(productRepository.getReferenceById(existingProductId)).thenReturn(product);
+		when(productRepository.getReferenceById(nonExistingProductId)).thenThrow(EntityNotFoundException.class);
+		
+		when(repository.save(any())).thenReturn(order);
+		when(orderItemRepository.saveAll(any())).thenReturn(new ArrayList<>(order.getItems()));
 	}
 
 	@Test
@@ -94,6 +124,30 @@ public class OrderServiceTests {
 		
 		assertThrows(ResourceNotFoundException.class, () -> {
 			OrderDTO result = service.findById(nonExistingOrderId);
+		});
+	}
+	
+	@Test
+	public void insertShouldReturnOrderDToWhenAdminLogged() {
+		when(userService.authenticated()).thenReturn(admin);		
+		OrderDTO result = service.insert(orderDTO);		
+		assertNotNull(result);		
+	}
+	
+	@Test
+	public void insertShouldReturnOrderDToWhenClientLogged() {
+		when(userService.authenticated()).thenReturn(client);		
+		OrderDTO result = service.insert(orderDTO);		
+		assertNotNull(result);		
+	}
+	
+	@Test
+	public void insertShouldThrowsUserNotFoundExceptionWhenUserNotLooged() {
+		doThrow(UsernameNotFoundException.class).when(userService).authenticated();
+		order.setClient(new User());
+		orderDTO = new OrderDTO(order);
+		assertThrows(UsernameNotFoundException.class, () -> {
+			OrderDTO result = service.insert(orderDTO);
 		});
 	}
 }
